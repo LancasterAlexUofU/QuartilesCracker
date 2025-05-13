cd37883f3db178e61169d6e52f908a1b3f91e58e
Alex Lancaster <u1505710@umail.utah.edu>
2025-05-12 22:24:00 -0600

    Added documentation to SolutionClicker and Chunk class

--- Chunk/Chunk.cs
+++ Chunk/Chunk.cs
@@ -2,11 +2,24 @@
 
 namespace Chunks
 {
+    /// <summary>
+    /// Class that holds information about word chunks in a quartile
+    /// </summary>
     public class Chunk
     {
+        /// <summary>
+        /// Letter data contained in a chunk
+        /// </summary>
         public string Letters { get; set; }
 
+        /// <summary>
+        /// Helper variable for Row getter and setter
+        /// </summary>
         private int row;
+
+        /// <summary>
+        /// Gets and sets the current row. When set, value is updated
+        /// </summary>
         public int Row
         {
             get => row;
@@ -17,7 +30,14 @@ namespace Chunks
             }
         }
 
+        /// <summary>
+        /// Helper variable for Column getter and setter
+        /// </summary>
         private int column;
+
+        /// <summary>
+        /// Gets and set the current row. When set, value is updated
+        /// </summary>
         public int Column
         {
             get => column;
@@ -28,11 +48,29 @@ namespace Chunks
             }
         }
 
+        /// <summary>
+        /// Position of the chunk in a 1D array
+        /// </summary>
         public int Value { get; private set; }
+
+        /// <summary>
+        /// Center position of the chunk on the screen
+        /// </summary>
         public Point CenterPos { get; set; }
 
+        /// <summary>
+        /// Largest amount of chunks that can be used to create a solution (aka the column width)
+        /// </summary>
         private int maxChunkSize;
 
+        /// <summary>
+        /// Constructor for a chunk object
+        /// </summary>
+        /// <param name="letters">The letter information for a chunk</param>
+        /// <param name="row">Row of the chunk in the quartiles grid, 0-indexed</param>
+        /// <param name="column">Column of the chunk in the quartiles grid, 0-indexed</param>
+        /// <param name="centerPos">Center position of the chunk on screen</param>
+        /// <param name="maxChunkSize">[Optional parameter] Changes the maximum amount of chunks that can be used to form a solution</param>
         public Chunk(string letters, int row, int column, Point centerPos, int maxChunkSize = 4)
         {
             Letters = letters;
@@ -42,6 +80,13 @@ namespace Chunks
             this.maxChunkSize = maxChunkSize;
         }
 
+        /// <summary>
+        /// Constructor for a chunk object, with Letters initialized to empty
+        /// </summary>
+        /// <param name="row">Row of the chunk in the quartiles grid, 0-indexed</param>
+        /// <param name="column">Column of the chunk in the quartiles grid, 0-indexed</param>
+        /// <param name="centerPos">Center position of the chunk on screen</param>
+        /// <param name="maxChunkSize">[Optional parameter] Changes the maximum amount of chunks that can be used to form a solution</param>
         public Chunk(int row, int column, Point centerPos, int maxChunkSize = 4)
         {
             Letters = string.Empty;
@@ -51,11 +96,23 @@ namespace Chunks
             this.maxChunkSize = maxChunkSize;
         }
 
+        /// <summary>
+        /// Given a value (which is a 1D array position), updates the rows and columns
+        /// 
+        /// <para>
+        /// Rows and Columns setters will also update the value
+        /// </para>
+        /// </summary>
+        /// <param name="value"></param>
         public void UpdateRowsAndColumns(int value)
         {
             Row = value / maxChunkSize;
             Column = value % maxChunkSize;
         }
+
+        /// <summary>
+        /// Updates the value to be the 1D array position of the new Rows and Columns position
+        /// </summary>
         private void UpdateValue()
         {
             Value = Row * maxChunkSize + Column;
--- SolutionClicker/SolutionClicker.cs
+++ SolutionClicker/SolutionClicker.cs
@@ -1,40 +1,117 @@
-﻿using Chunks;
+﻿using System.Runtime.InteropServices;
+using Chunks;
 using QuartilesToText;
 using Quartiles;
-using System.Runtime.InteropServices;
 
+/// <summary>
+/// Clicks on the screen to solve a quartile game
+/// </summary>
 public class SolutionClicker
 {
+    /// <summary>
+    /// Extractor that uses OCR to extract the letters from the quartiles game
+    /// </summary>
     private QTT extractor;
+
+    /// <summary>
+    /// Solver that finds the solutions for the quartile
+    /// </summary>
     private QuartilesCracker solver;
 
+    /// <summary>
+    /// Name of the image to be scanned for letters, including the file extension
+    /// </summary>
     private string imageName;
 
+
+
+    /// <summary>
+    /// The number of columns in the quartile. 1-Indexed
+    /// </summary>
     private int columns;
+
+    /// <summary>
+    /// The number of rows in the quartile. 1-Indexed
+    /// </summary>
     private int rows;
+
+    /// <summary>
+    /// Holds the row index of the last unoccupied row. 0-Indexed
+    /// 
+    /// <para>
+    /// Whenever a max chunk size solution is found, the chunks are moved to the last unoccupied row
+    /// </para>
+    /// </summary>
     private int lastUnoccupiedRowIndex;
 
-    Point topLeft;
-    Point bottomRight;
+    /// <summary>
+    /// Top left corner of the quartile grid. Used for OCR scanning
+    /// </summary>
+    private Point topLeft;
 
-    private int score;
-    Point topLeftScore;
-    Point bottomRightScore;
+    /// <summary>
+    /// Bottom right corner of the quartile grid. Used for OCR scanning
+    /// </summary>
+    private Point bottomRight;
 
-    private Point checkSolution;
-    private Point closePopup;
-    private bool expertPopupClosed;
-    private bool perfectPopupClosed;
-
+    /// <summary>
+    /// List of chunks that are used to store the letters and their center positions
+    /// </summary>
     private List<Chunk> wordChunks;
 
+
+
+    /// <summary>
+    /// Current score of the quartile game. Used for checking if the solution is valid
+    /// </summary>
+    private int score;
+
+    /// <summary>
+    /// Top left corner of the score area. Ensure corner contains area for 3-digit numbers. Used for OCR scanning
+    /// </summary>
+    private Point topLeftScore;
+
+    /// <summary>
+    /// Bottom right corner of the score area. Ensure corner contains area for 3-digit numbers. Used for OCR scanning
+    /// </summary>
+    private Point bottomRightScore;
+
+
+
+    /// <summary>
+    /// Center position for the check mark button
+    /// </summary>
+    private Point checkSolution;
+
+    /// <summary>
+    /// Center position for the 'x' button to close a popup
+    /// </summary>
+    private Point closePopup;
+
+    /// <summary>
+    /// Boolean to check if the expert level popup has been closed
+    /// </summary>
+    private bool expertPopupClosed;
+
+    /// <summary>
+    /// Boolean to check if the perfect level popup has been closed
+    /// </summary>
+    private bool perfectPopupClosed;
 
     public static void Main(string[] args)
     {
         var SolutionClicker = new SolutionClicker("quartiles-2024-06-16.png");
         SolutionClicker.PrintMousePostition();
+
+        SolutionClicker.RunAutoSolver();
+
+        Console.WriteLine($"Final score: {SolutionClicker.score}");
     }
 
+    /// <summary>
+    /// Constructor for the SolutionClicker class
+    /// </summary>
+    /// <param name="imageName">Image filename in QuartilesImages to scan</param>
     public SolutionClicker(string imageName)
     {
         extractor = new QTT(imageName);
@@ -45,6 +122,7 @@ public class SolutionClicker
         columns = solver.MAX_CHUNKS;
         rows = solver.MAX_LINES;
         lastUnoccupiedRowIndex = rows - 1;
+        wordChunks = new List<Chunk>();
 
         // https://www.quartilesgame.org, 100% Vivaldi
         topLeft = new Point(630, 320);
@@ -57,20 +135,27 @@ public class SolutionClicker
         checkSolution = new Point(1040, 615);
         closePopup = new Point();
         expertPopupClosed = false;
-        
+    }
 
-        wordChunks = new List<Chunk>();
-
-
+    /// <summary>
+    /// Automatically solves the quartile by scanning and clicking on the screen
+    /// </summary>
+    public void RunAutoSolver()
+    {
         SetChunksCenterPosition();
         SetChunksLetters();
         SolveQuartile();
         ClickMaxChunkSize();
         ClickAllChunks();
-
-        Console.WriteLine($"Final score: {score}");
     }
 
+    /// <summary>
+    /// Sets the rows, column, and center position values for each word chunk
+    /// 
+    /// <para>
+    /// Adds chunks from left to right, top to bottom
+    /// </para>
+    /// </summary>
     private void SetChunksCenterPosition()
     {
         int cellWidth = (bottomRight.X - topLeft.X) / columns;
@@ -91,6 +176,13 @@ public class SolutionClicker
         }
     }
 
+    /// <summary>
+    /// Scans the quartile grid and sets the letters in the correct positions
+    /// 
+    /// <para>
+    /// ExtractChunks returns chunks from left to right, top to bottom
+    /// </para>
+    /// </summary>
     private void SetChunksLetters()
     {
         extractor.ExtractChunks();
@@ -101,20 +193,25 @@ public class SolutionClicker
         }
     }
 
+    /// <summary>
+    /// Finds solutions to quartile game, stored in solver.results
+    /// </summary>
     private void SolveQuartile()
     {
         solver.chunks = extractor.chunks;
         solver.QuartilesDriver();
     }
 
+    /// <summary>
+    /// Left clicks on a given list of chunks
+    /// </summary>
+    /// <param name="chunkList"></param>
     private void ClickOnChunks(List<string> chunkList)
     {
-        // For each key in wordChunkMapping, extract the list of chunks
-        // For each chunk, find the equivalent letters in wordChunks
-        // Then, add the center position of that chunk to solutionClicks
 
         foreach(var chunk in chunkList)
         {
+            // Finds the matching chunk in wordChunks
             Chunk matchedChunk = wordChunks.FirstOrDefault(c => c.Letters == chunk);
 
             if (matchedChunk != null)
@@ -129,12 +226,19 @@ public class SolutionClicker
             }
         }
     }
+
+    /// <summary>
+    /// Clicks the check solution button 
+    /// </summary>
     private void ClickCheckSolution()
     {
         Cursor.Position = checkSolution;
         LeftClick();
     }
 
+    /// <summary>
+    /// Clicks on the 'x' to close any popup that appears
+    /// </summary>
     private void ClickClosePopup()
     {
         Cursor.Position = closePopup;
@@ -143,6 +247,9 @@ public class SolutionClicker
         LeftClick();
     }
 
+    /// <summary>
+    /// Call to simulate a left click
+    /// </summary>
     private void LeftClick()
     {
         mouse_event(MouseEventFlags.LeftDown, 0, 0, 0, UIntPtr.Zero);
@@ -150,12 +257,16 @@ public class SolutionClicker
         mouse_event(MouseEventFlags.LeftUp, 0, 0, 0, UIntPtr.Zero);
     }
 
+    /// <summary>
+    /// Takes a screenshot of the current screen to see if the score has increased.
+    /// 
+    /// <para>
+    /// If the score increases, this indicates a solution is valid
+    /// </para>
+    /// </summary>
+    /// <returns>True if the score increased, false otherwise</returns>
     private bool IsSolutionValid()
     {
-        // Checking using OCR if the score increased. If so, the solution is valid
-        // Really only need to check for max chunk size solutions
-        // For now, just returning true
-
         int previousScore = score;
         score = GetScore();
 
@@ -167,6 +278,14 @@ public class SolutionClicker
         return false;
     }
 
+    /// <summary>
+    /// Takes a screenshot of the current score and returns the value
+    /// 
+    /// <para>
+    /// If the score is empty, it could be due to a popup. The method will attempt to close the popup and try again.
+    /// </para>
+    /// </summary>
+    /// <returns></returns>
     private int GetScore()
     {
         // Takes a screenshot of the score area and uses OCR to extract the score
@@ -231,13 +350,26 @@ public class SolutionClicker
         }
     }
 
+    /// <summary>
+    /// Calls GetScore to scan the screen and updates the score variable to reflect the new score.
+    /// </summary>
     private void UpdateScore()
     {
         int newScore = GetScore();
         score = newScore;
     }
 
-
+    /// <summary>
+    /// Only clicks on quartile solutions using the max chunk size
+    /// 
+    /// <para>
+    /// Since the chunks only move around for max sized solution, this method is separate
+    /// </para>
+    /// 
+    /// <para>
+    /// Once all valid max chunk size solutions have been found, this program will stop
+    /// </para>
+    /// </summary>
     private void ClickMaxChunkSize()
     {
         // Keeps track of how many solutions have been solved using the maximum amount of chunks
@@ -273,6 +405,9 @@ public class SolutionClicker
         }
     }
 
+    /// <summary>
+    /// Clicks on all chunks of max chunk size - 1
+    /// </summary>
     private void ClickAllChunks()
     {
         int startIndex = GetFirstNonMaxChunkSizeIndex();
@@ -310,10 +445,15 @@ public class SolutionClicker
         return solver.wordChunkMapping.Count - 1;
     }
 
+    /// <summary>
+    /// Calculates the new position of all chunk after tiles rearrange from a max chunk size solution
+    /// </summary>
+    /// <param name="validMaxChunks">The chunks that formed a valid solution</param>
     private void CalculateNewValues(List<string> validMaxChunks)
     {
         var sortedChunks = new SortedList<int, Chunk>();
 
+        // Adds chunks to a sorted list ordered by their position value in the 2D grid
         foreach (var chunk in validMaxChunks)
         {
             Chunk matchedChunk = wordChunks.FirstOrDefault(c => c.Letters == chunk);
@@ -329,7 +469,8 @@ public class SolutionClicker
             }
         }
 
-        // Calculates and updates the value offsets for each chunk not in the sorted list
+        // Adds each chunk to the list to find where its position lies to calculate its offset
+        // Then, updates the chunk to be at its new position
         foreach (var chunk in wordChunks)
         {
             try
@@ -341,9 +482,11 @@ public class SolutionClicker
                 chunk.UpdateRowsAndColumns(chunk.Value - offset);
             }
 
+            // Caught when adding duplicate keys to a sorted list, skips over
             catch (ArgumentException) { }
         }
 
+        // Adds validMaxChunks to the last unoccupied row
         for (int column = 0; column < columns; column++)
         {
             Chunk chunk = sortedChunks.GetValueAtIndex(column);
@@ -352,6 +495,20 @@ public class SolutionClicker
         }
 
         lastUnoccupiedRowIndex--;
+    }
+
+    /// <summary>
+    /// Prints the mouse position for debugging purposes
+    /// </summary>
+    private void PrintMousePostition()
+    {
+        Console.WriteLine("Press ESC to stop.");
+        while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+        {
+            Point pos = Cursor.Position;
+            Console.WriteLine($"X: {pos.X}, Y: {pos.Y}");
+            Thread.Sleep(500); // Update every half second
+        }
     }
 
     [Flags]
@@ -363,15 +520,4 @@ public class SolutionClicker
 
     [DllImport("user32.dll", SetLastError = true)]
     static extern void mouse_event(MouseEventFlags dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
-
-    private void PrintMousePostition()
-    {
-        Console.WriteLine("Press ESC to stop.");
-        while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
-        {
-            Point pos = Cursor.Position;
-            Console.WriteLine($"X: {pos.X}, Y: {pos.Y}");
-            Thread.Sleep(500); // Update every half second
-        }
-    }
 }
